93ad6a45d4d7e81a8800f49c013099895bfba86c
Bartosz Milejski <mil_bar@hotmail.de>
2016-08-29 13:40:06 +0200

    Add support for material queries

--- Server/monitoring.php
+++ Server/monitoring.php
@@ -6,7 +6,49 @@
 	<style type="text/css">
 		body {
 			font-family: Verdana;
-			position: relative;
+			height: auto;
+		}
+		/* Header enthält Logo und Überschrift */
+		#header {
+			height: 70px;
+			vertical-align: middle;
+			margin-bottom: 5px;
+		}
+		#header img {
+			float: left;
+		}
+		#header h1 {
+			display: inline-block;
+			padding-left: 10px;
+			margin-top: 18px;
+		}
+		/* Im Contentwrapper sind beide Unterüberschriften mit dazugehörigen Subcontentwrappern
+		(#warehouse, #devices) und den jeweiligen Tabellen (#maintable, #devtable) */
+		#contentwrapper {
+			padding: 5px;
+			padding-bottom: 15px;
+			clear: both;
+			width: auto;
+			height: auto;
+			border: 1px solid black;
+		}
+		#warehouse {
+			display: inline-block;
+			margin-left: 5px;
+			margin-top: 0px;
+			width: 60%;
+			height: auto;
+		}
+		#devices {
+			display: inline-block;
+			margin-left:80px;
+			margin-top:0px;
+			width: 20%;
+			height: auto;
+		}
+		#maintable, #devtable {
+			width: 100%;
+			height: auto;
 		}
 		table, td {
 			border: 1px solid black;
@@ -15,46 +57,110 @@
 		td {
 			text-align: left;
 			vertical-align: top;
-		}
-		#contentwrapper {
-			position: absolute;
-			width: 100%;
-			height: auto;
-			background: yellow;
-		}
-		#warehouse {
-			float: left;
-			position: relative;
-			margin-left: 10px;
-			margin-top: 0px;
-			width: 60%;
-			height: auto;
-			background-color: red;
-		}
-		#devices {
-			float: right;
-			position: relative;
-			margin-left:10px;
-			margin-top:0px;
-			width: 20%;
-			height: 50%;
-			display: inline-block;
-			background-color: green;
-		}
-		#maintable, #devtable {
-			width: 100%;
-			height: 100%;
+			overflow: hidden;
 		}
 		#maintable td {
+			height: 150px;
 			width: 33%;
-			height: 100px;
 		}
 		#devtable td {
-			width: 33%;
 			height: 150px;
+		}
+		/* Dies ist der Inhalt eines td Elements. Ein td enthält ein span.id Element,
+		 * das die ID des Feldes aus aus der DB anzeigt, sowie ein span.amount Element
+		 * in das die Anzahl an Positionen (Device oder Lager) hineingeschrieben wird. */
+		.id {
+			font-weight: bold;
+		}
+		.amount {
+			
+		}
+		/* .s1, .s2, .s3 stehen für die 3 Schraubenschlüssel Bilder */
+		.s1, .s2, .s3 {
+			position: absolute;
+			display: none;
+		}
+		.s1 {
+			margin-left: 10px;
+			margin-top: 10px;
+		}
+		.s2 {
+			margin-left: 20px;
+			margin-top: 20px;
+		}
+		.s3 {
+			margin-left: 30px;
+			margin-top: 30px;
+		}
+		#footer {
+			padding-top: 50px;
+			clear: both;
+			text-align: right;
 		}
 	</style>
 	<script>
+		// Einstellbare Parameter
+		timeoutMaterial = 2000;
+		timeoutPositions = 10000;
+		fadeSpeed = 1000;
+		
+		/* getMaterials lädt mit einem AJAX-Request eine Zuordnung von Material zu
+		 * Lagerposition. Diese wird bei erfolgreichem Request ausgewertet und es
+		 * wird die Anzahl der Materialien auf einer Position in die jeweilge Position
+		 * geschrieben. Zusätzlich wird eine Veranschaulichung durchgeführt indem
+		 * zwischen 0 und 3 Bildern an der jeweiligen Position angezeigt werden. */
+		function getMaterials() {
+			$.ajax({
+				type: "POST",
+				url: 'position_query.php',
+				data: {
+					submit: true,
+					query: "mat"
+				},
+				success: function(data) {
+					// Wenn es eine Änderung bei der Position von einem Material gab,
+					// werden alle Materialien neu gezeichnet
+					if (matJsonData != data) {
+						var matToPosAssignment = jQuery.parseJSON(data);
+						var keys = Object.keys(matToPosAssignment);
+						$('#maintable tr, #devtable tr').children().each(function() {
+							var matCountPerPos = 0;
+							// Anzahl der Materialien für Lagerposition (this) herausfinden
+							for (var i = 0; i < keys.length; i++) {
+								if ($(this).attr('id') == "t"+matToPosAssignment[keys[i]]) {
+									matCountPerPos++;
+								}
+							}
+							// Wenn mindestens ein Material an dieser Position ist
+							if (matCountPerPos > 0) {
+								$(this).children('.amount').html('Anzahl Objekte: ' + matCountPerPos + '<br>');
+								if (matCountPerPos > 2) {
+									$(this).children('img').fadeIn(fadeSpeed);
+								} else if (matCountPerPos > 1) {
+									$(this).children('img.s1, img.s2').fadeIn(fadeSpeed);
+									$(this).children('img.s3').fadeOut(fadeSpeed);
+								} else {
+									$(this).children('img.s1').fadeIn(fadeSpeed);
+									$(this).children('img.s2, img.s3').fadeOut(fadeSpeed);
+								}
+							} else {
+								$(this).children('img').fadeOut(fadeSpeed);
+								$(this).children('.amount').html('');
+							}
+						});
+					}
+					// Speichern des Vergleichswertes für die Überprüfung ob sich etwas geändert hat
+					matJsonData = data;
+				},
+				complete: function() {
+					setTimeout(getMaterials, timeoutMaterial);
+				}
+			});
+		}
+		
+		/* getNewLPositions lädt mit einem AJAX-Request eine Tabelle von Lagerpositionen,
+		 * in der sich bereits die IDs der Lagerpositionen befinden. Diese Tabelle wird
+		 * später von getMaterials() gefüllt */
 		function getNewLPositions() {
 			$.ajax({
 				type: "POST",
@@ -64,13 +170,27 @@
 					query: "lpos"
 				},
 				success: function(data) {
-					$('#maintable').html(data);
+					// Wenn es eine Veränderung gab
+					if (data != lagerPosData) {
+						$('#maintable').html(data);
+						// Zähle die Anzahl von Positionen und schreibe sie in die Überschrift
+						var lagerPosCounter = 0;
+						$('#maintable tr').children().each(function() {
+							lagerPosCounter++;
+						});
+						$('#warehouse h2').append(': ' + lagerPosCounter);
+					}
+					lagerPosData = data;
 				},
 				complete: function() {
-					setTimeout(getNewLPositions, 10000);
+					setTimeout(getNewLPositions, timeoutPositions);
 				}
 			});
 		}
+		
+		/* getNewDPositions lädt mit einem AJAX-Request eine Tabelle von Gerätepositionen,
+		 * in der sich bereits die IDs der Gerätepositionen befinden. Diese Tabelle wird
+		 * später von getMaterials() gefüllt */
 		function getNewDPositions() {
 			$.ajax({
 				type: "POST",
@@ -80,25 +200,44 @@
 					query: "dpos"
 				},
 				success: function(data) {
-					$('#devtable').html(data);
+					// Wenn es eine Veränderung gab
+					if (data != devPosData) {
+						$('#devtable').html(data);
+						// Zähle die Anzahl von Positionen und schreibe sie in die Überschrift
+						var deviceCounter = 0;
+						$('#devtable tr').children().each(function() {
+							deviceCounter++;
+						});
+						$('#devices h2').append(': ' + deviceCounter);
+					}
+					devPosData = data;
 				},
 				complete: function() {
-					setTimeout(getNewDPositions, 10000);
+					setTimeout(getNewDPositions, timeoutPositions);
 				}
 			});
 		}
 		
 		$(document).ready(function(){
-		setTimeout(getNewLPositions, 1000);
-		setTimeout(getNewDPositions, 1000);
+			// Initialisiere globale Variablen
+			matJsonData = "";
+			lagerPosData = "";
+			devPosData = "";
+			// Starte AJAX-Loop
+			setTimeout(getNewLPositions, 0);
+			setTimeout(getNewDPositions, 0);
+			setTimeout(getMaterials, 1000);
 		});
 	</script>
 </head>
 <body>
-	<h1>BTaIT Lager Monitoring</h1>
+	<div id="header">
+		<img src="DiK_Logo.jpg" alt="DiK Logo" class="dik_logo" width="80" height="70">
+		<h1>BTaIT Lager Monitoring</h1>
+	</div>
 	<div id="contentwrapper">
 		<div id="warehouse">
-			<h2>Lager</h2>
+			<h2>Lagerpositionen</h2>
 			<table id="maintable">
 				<tr>
 					<td>1</td>
@@ -114,8 +253,12 @@
 			<h2>Geräte</h2>
 			<table id="devtable">
 				<tr><td>Device 1</td></tr>
+				<tr><td>Device 2</td></tr>
 			</table>
 		</div>
+	</div>
+	<div id="footer">
+		<div id="phpResponse"></div>
 	</div>
 </body>
 </html>
--- Server/position_query.php
+++ Server/position_query.php
@@ -2,6 +2,7 @@
 if (isset($_POST['submit']) && $_POST['submit'] == true) {
 	// Connect to DB
 	include("dbconnect.php");
+	// Decide which query should be done
 	if (isset($_POST['query'])) {
 		switch ($_POST['query']) {
 			case "lpos":
@@ -9,6 +10,9 @@ if (isset($_POST['submit']) && $_POST['submit'] == true) {
 				break;
 			case "dpos":
 				do_devices_pos_query($connection);
+				break;
+			case "mat":
+				do_materials_query($connection);
 				break;
 			default:
 				echo "Dont manipulate POST parameters, bitch.";
@@ -19,7 +23,28 @@ if (isset($_POST['submit']) && $_POST['submit'] == true) {
 
 
 // Function definitions
+// -----------------------------------------------------------
 
+// Returns a JSON-Object containing an assignment of Material.ID -> Lagerposition.ID
+function do_materials_query($connection) {
+	$sql = "SELECT M.ID as mat_id, L.ID as pos_id, M.Position FROM Material M, Lagerposition L WHERE M.Position = L.ID";
+	
+	if ($results = $connection->query($sql)) {
+		$material_amount = mysqli_num_rows($results);
+		if ($material_amount > 0) {
+			$data = array();
+			for ($mat_counter = 0; $mat_counter < $material_amount; $mat_counter++) {
+				$row = $results->fetch_assoc();
+				$mat_id = $row["mat_id"];
+				$data[$mat_id] = $row["pos_id"];
+			}
+			echo json_encode($data);
+		}
+	}
+}
+
+// Returns a table containg as many columns as warehouse positions exist. Every column has an
+// ID (t0 - tX where X is the amount of colums - 1).
 function do_warehouse_pos_query($connection) {
 	$results_per_col = 3;
 	$sql = "SELECT * FROM Lagerposition;";
@@ -28,7 +53,6 @@ function do_warehouse_pos_query($connection) {
 	
 		$position_amount = mysqli_num_rows($results);
 		$row_amount = intval($position_amount/$results_per_col) + 1;
-		//echo "Position amount: $position_amount, Row amount: $row_amount <br>";
 	  	
 		if ($position_amount > 0) {
 			echo '<tr>';
@@ -36,8 +60,13 @@ function do_warehouse_pos_query($connection) {
 				$row = $results->fetch_assoc();
 				if ($row["Typ"] == "L") {
 					$id = $row["ID"];
-					echo "<td id='t$id'>$id</td>";
-					if ($pos_counter % 3 == 0) {
+					echo "<td id='t$id'><span class='id'>$id </span>";
+					echo "<span class='amount'></span>";
+					echo "<img src='spanner-klein.png' alt='Schraubenschlüssel' class='s1'>";
+					echo "<img src='spanner-klein.png' alt='Schraubenschlüssel' class='s2'>";
+					echo "<img src='spanner-klein.png' alt='Schraubenschlüssel' class='s3'>";
+					echo "</td>";
+					if ($pos_counter % $results_per_col == 0) {
 						echo "</tr><tr>";
 					}
 				}
@@ -51,22 +80,27 @@ function do_warehouse_pos_query($connection) {
 	}
 	
 }
+
+// Returns a table containg as many columns as device positions exist. Every column has an
+// ID (t0 - tX where X is the amount of colums - 1).
 function do_devices_pos_query($connection) {
-	$results_per_col = 3;
 	$sql = "SELECT * FROM Lagerposition;";
 
 	if ($results = $connection->query($sql)) {
 	
 		$position_amount = mysqli_num_rows($results);
-		$row_amount = intval($position_amount/$results_per_col) + 1;
-		//echo "Position amount: $position_amount, Row amount: $row_amount <br>";
 	  	
 		if ($position_amount > 0) {
 			for ($pos_counter = 1; $pos_counter <= $position_amount; $pos_counter++) {
 				$row = $results->fetch_assoc();
 				if ($row["Typ"] == "D") {
 					$id = $row["ID"];
-					echo "<tr><td id='t$id'>$id</td></tr>";
+					echo "<tr><td id='t$id'><span class='id'>$id </span>";
+					echo "<span class='amount'></span>";
+					echo "<img src='spanner-klein.png' alt='Schraubenschlüssel' class='s1'>";
+					echo "<img src='spanner-klein.png' alt='Schraubenschlüssel' class='s2'>";
+					echo "<img src='spanner-klein.png' alt='Schraubenschlüssel' class='s3'>";
+					echo "</td></tr>";
 				}
 			}
 		  	$results->free();
